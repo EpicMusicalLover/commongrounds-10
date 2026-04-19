c911a06161f4e6ae7f5d1d6896b5fe53f2f62258
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-04-19 21:35:35 +0800

    modified the urls and views of accounts app

--- commongrounds/accounts/urls.py
+++ commongrounds/accounts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import ProfileUpdateView, RegisterView
 
-app_name = "accounts"
-
 urlpatterns=[
     path("<str:username>/", ProfileUpdateView.as_view(), name="profile-update"),
     path("register/", RegisterView.as_view(), name="register"),
--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -6,3 +6,14 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
     fields = ['display_name']
     template_name = 'profile_update.html'
+
+class RegisterView(CreateView): #temporary
+    model = User
+    form_class = UserCreationForm
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        user = form.save()
+        Profile.objects.create(user=user)
+        return super().form_valid(form)
