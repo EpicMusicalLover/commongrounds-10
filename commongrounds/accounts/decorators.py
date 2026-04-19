2585a9847267e19cda1d10a109783982a4f63a7d
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-04-19 19:28:58 +0800

    added another accounts path in urls.py for app level

---
+++ commongrounds/accounts/decorators.py
@@ -0,0 +1,22 @@
+from functools import wraps
+from django.shortcuts import redirect
+from django.core.exceptions import PermissionDenied
+from .models import Profile
+
+def role_required(required_role):
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(request, *args, **kwargs):
+
+            if not request.user.is_authenticated:
+                return redirect('login')
+            try:
+                profile = Profile.objects.get(user=request.user)
+            except Profile.DoesNotExist:
+                raise PermissionDenied
+            if profile.role != required_role:
+                raise PermissionDenied 
+            return view_func(request, *args, **kwargs)
+
+        return wrapper
+    return decorator
--- commongrounds/accounts/urls.py
+++ commongrounds/accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 from .views import ProfileUpdateView
 
 app_name = "accounts"
 
 urlpatterns=[
     path("<str:username>/", ProfileUpdateView.as_view(), name="profile-update"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
