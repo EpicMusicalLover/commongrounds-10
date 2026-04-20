de2959e1eeb19430ac8e4b1aeb1088adc34f17ed
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-04-20 22:10:30 +0800

    added accounts app and modified models, urls, and views, for the create, update, and cart views and also added the cart template

--- commongrounds/merchstore/models.py
+++ commongrounds/merchstore/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from decimal import Decimal
+
 # from django.urls import reverse
 
 
@@ -23,7 +24,7 @@ class Product(models.Model):
         null=True,
     )
     owner = models.ForeignKey(
-        'accounts.Profile',
+        "accounts.Profile",
         on_delete=models.CASCADE,
         null=True,
     )
@@ -34,21 +35,21 @@ class Product(models.Model):
     )
     description = models.TextField()
     price = models.DecimalField(
-        max_digits=10, 
+        max_digits=10,
         decimal_places=2,
-        validate=[MinValueValidator(Decimal('0.01'))]
+        # validate=[MinValueValidator(Decimal('0.01'))]
     )
     stock = models.IntegerField(
-        validate = [MinValueValidator(0)]
+        # validate = [MinValueValidator(0)]
     )
     status = models.CharField(
         max_length=255,
-        choices = [
-            ('available','Available'),
-            ('on_sale', 'On sale'),
-            ('out_of_stock', 'Out of stock'),
+        choices=[
+            ("available", "Available"),
+            ("on_sale", "On sale"),
+            ("out_of_stock", "Out of stock"),
         ],
-        default = 'available',
+        default="available",
     )
 
     class Meta:
@@ -56,10 +57,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Transaction(models.Model):
     buyer = models.ForeignKey(
-        'accounts.Profile',
+        "accounts.Profile",
         on_delete=models.SET_NULL,
         null=True,
     )
@@ -67,21 +69,22 @@ class Transaction(models.Model):
         Product,
         on_delete=models.CASCADE,
         null=True,
-    ) 
+    )
     amount = models.IntegerField(
-        validate = [MinValueValidator(1)]
+        # validate = [MinValueValidator(1)]
     )
     status = models.CharField(
-        max_length = 255,
-        choices = [
-            ('on_cart', 'On cart'),
-            ('to_pay', 'To Pay'),
-            ('to_ship', 'To Ship'),
-            ('to_receive', 'To Receive'),
-            ('delivered', 'Delivered')
-        ]
+        max_length=255,
+        choices=[
+            ("on_cart", "On cart"),
+            ("to_pay", "To Pay"),
+            ("to_ship", "To Ship"),
+            ("to_receive", "To Receive"),
+            ("delivered", "Delivered"),
+        ],
     )
     created_on = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
 
