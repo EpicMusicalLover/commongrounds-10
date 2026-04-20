1d7506fcffb5c36833b629ad3aad0e92781d89f8
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-04-20 17:47:27 +0800

    added forms.py and modified views.py for registerview

--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import RegisterForm
 from .models import Profile
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
@@ -7,12 +9,11 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     fields = ['display_name']
     template_name = 'profile_update.html'
 
-class RegisterView(CreateView): #temporary
-    model = User
-    form_class = UserCreationForm
-    template_name = "accounts/register.html"
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "register.html"
     success_url = reverse_lazy("login")
-
+    
     def form_valid(self, form):
         user = form.save()
         Profile.objects.create(user=user)
