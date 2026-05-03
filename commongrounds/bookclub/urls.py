fdffc421a67fddd952d99e4984aad0d9159de8aa
annapaulaaranas-stack <anna.paula.aranas@student.ateneo.edu>
2026-05-04 01:54:47 +0800

    updated the project-level & app-level urls

--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookBorrowView
 
 
 app_name = "bookclub"
@@ -8,4 +8,7 @@ app_name = "bookclub"
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book_list'),
     path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('book/add/', BookCreateView.as_view(), name='book_create'),
+    path('book/<int:pk>/edit/', BookUpdateView.as_view(), name='book_update'),
+    path('book/<int:pk>/borrow/', BookBorrowView.as_view(), name='book_borrow'),
 ]
