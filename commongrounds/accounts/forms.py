abe7e21e8bc2f38b36e3b81167567c65346fb150
annapaulaaranas-stack <anna.paula.aranas@student.ateneo.edu>
2026-05-03 14:02:43 +0800

    added accounts app and update installed apps

---
+++ commongrounds/accounts/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    role = forms.ChoiceField(
+        choices=[
+            ("User", "User"),
+            ("Market Seller", "Market Seller"),
+            ("Event Organizer", "Event Organizer"),
+            ("Book Contributor", "Book Contributor"),
+            ("Project Creator", "Project Creator"),
+            ("Commission Maker", "Commission Maker"),
+        ],
+    )
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
---
+++ commongrounds/accounts/views.py
@@ -0,0 +1,31 @@
+from django.views.generic import UpdateView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import RegisterForm
+from .models import Profile
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    fields = ["display_name"]
+    template_name = "profile_update.html"
+
+    def get_object(self):
+        return self.request.user.profile
+
+    def get_success_url(self):
+        return f"/accounts/{self.object.user.username}/"
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "register.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        user_save = form.save()
+        Profile.objects.get_or_create(
+            user=user_save,
+            role=form.cleaned_data["role"]
+        )
+        return super().form_valid(form)
