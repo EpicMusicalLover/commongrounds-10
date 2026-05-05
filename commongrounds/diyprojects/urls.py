dd04f581198f73140e6e919879455b0ec2ce7082
Larc956 <carl.basco@student.ateneo.edu>
2026-05-06 00:01:37 +0800

    debugged urls.

--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
-
-from .views import (ProjectCreateView, ProjectDetailView, ProjectListView,
-                    ProjectUpdateView)
+from .views import (
+    ProjectListView,
+    ProjectDetailView,
+    ProjectCreateView,
+    ProjectUpdateView,
+)
 
 app_name = "diyprojects"
 
 urlpatterns = [
     path("projects/", ProjectListView.as_view(), name="project_list"),
-    path(
-        "project/<int:pk>/", ProjectDetailView.as_view(),
-        name="project_detail",
-    ),
+    path("project/<int:pk>/", 
+         ProjectDetailView.as_view(), 
+         name="project_detail"),
     path("project/add/", ProjectCreateView.as_view(), name="project_create"),
-    path("project/<int:pk>/edit/",
-         ProjectUpdateView.as_view(), name="project_update"),
+    path("project/<int:pk>/edit/", ProjectUpdateView.as_view(), name="project_update"),
 ]
