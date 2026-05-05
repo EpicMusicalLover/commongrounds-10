43b0b282f05fc52b238c7bfd147d74b90776aab4
Kandice <april.kandice.pauline.cuizon@student.ateneo.edu>
2026-05-05 13:54:43 +0800

    Modified CommissionListView in views.py

--- commongrounds/commissions/views.py
+++ commongrounds/commissions/views.py
@@ -1,11 +1,23 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-from .models import Commission
+from django.views.generic import DetailView, CreateView, UpdateView, ListView
+from .models import Commission, Job, JobApplication
 
 
 class CommissionListView(ListView):
     model = Commission
     template_name = "commission_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["your_commissions"] = Commission.objects.filter(
+                maker=self.request.user.profile
+            )
+            context["your_applications"] = JobApplication.objects.filter(
+                applicant=self.request.user.profile
+            )
+        else:
+            context["other_commissions"] = Commission.objects.all()
+        return context
 
 
 class CommissionDetailView(DetailView):
