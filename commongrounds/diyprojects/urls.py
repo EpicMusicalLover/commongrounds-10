ddba784a1626c84b28690a24b82ab78813ec9c0e
Larc956 <carl.basco@student.ateneo.edu>
2026-05-06 00:10:16 +0800

    fixed files according to pep8 coding style

--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -10,9 +10,10 @@ app_name = "diyprojects"
 
 urlpatterns = [
     path("projects/", ProjectListView.as_view(), name="project_list"),
-    path("project/<int:pk>/", 
-         ProjectDetailView.as_view(), 
+    path("project/<int:pk>/",
+         ProjectDetailView.as_view(),
          name="project_detail"),
     path("project/add/", ProjectCreateView.as_view(), name="project_create"),
-    path("project/<int:pk>/edit/", ProjectUpdateView.as_view(), name="project_update"),
+    path("project/<int:pk>/edit/", ProjectUpdateView.as_view(),
+         name="project_update"),
 ]
