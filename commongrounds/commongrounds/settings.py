27e4d77166dbe39c29cd3743024f8fd3a152165e
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-05-07 16:40:05 +0800

    added the trusted origin to settings

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "commongrounds-10-production.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://commongrounds-10-production.up.railway.app",
+]
 
 
 # Application definition
