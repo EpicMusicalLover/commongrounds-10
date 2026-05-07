89c8342bd08c7fa5708186ee906e38a4f24a4adf
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-05-07 20:27:41 +0800

    fixed xonflict errors

---
+++ commongrounds/commissions/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.forms import inlineformset_factory
+from .models import Commission, Job, JobApplication
+
+
+class CommissionForm(forms.ModelForm):
+    class Meta:
+        model = Commission
+        fields = [
+            "title",
+            "description",
+            "commission_type",
+            "people_required",
+            "status",
+        ]
+        widget = {"status": forms.Select()}
+
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = [
+            "role",
+            "manpower_required",
+            "status",
+        ]
+
+
+JobFormSet = inlineformset_factory(
+    Commission, Job, fields=['role', 'manpower_required'], extra=2
+    )
+
+
+class JobApplicationForm(forms.ModelForm):
+    class Meta:
+        model = JobApplication
+        fields = []
