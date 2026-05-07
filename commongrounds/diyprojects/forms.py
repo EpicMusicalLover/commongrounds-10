d91a9fed3510426b6ead44ecc18980a29205c39e
Nathaniel Joaquin Quijencio <natesjoaquin@gmail.com>
2026-05-07 20:25:16 +0800

    fixed conflict errors

--- commongrounds/diyprojects/forms.py
+++ commongrounds/diyprojects/forms.py
@@ -1,37 +1,21 @@
 from django import forms
-from django.forms import inlineformset_factory
-from .models import Commission, Job, JobApplication
+from .models import Project, ProjectRating, ProjectReview
 
 
-class CommissionForm(forms.ModelForm):
+class ProjectForm(forms.ModelForm):
     class Meta:
-        model = Commission
-        fields = [
-            "title",
-            "description",
-            "commission_type",
-            "people_required",
-            "status",
-        ]
-        widget = {"status": forms.Select()}
+        model = Project
+        fields = ["title", "category", "description", "materials", "steps"]
 
 
-class JobForm(forms.ModelForm):
+class ProjectReviewForm(forms.ModelForm):
     class Meta:
-        model = Job
-        fields = [
-            "role",
-            "manpower_required",
-            "status",
-        ]
+        model = ProjectReview
+        fields = ["comment", "image"]
 
 
-JobFormSet = inlineformset_factory(
-    Commission, Job, fields=['role', 'manpower_required'], extra=2
-    )
-
-
-class JobApplicationForm(forms.ModelForm):
+class ProjectRatingForm(forms.ModelForm):
     class Meta:
-        model = JobApplication
-        fields = []
+        model = ProjectRating
+        fields = ["score"]
+        widgets = {"score": forms.NumberInput(attrs={"min": 1, "max": 10})}
